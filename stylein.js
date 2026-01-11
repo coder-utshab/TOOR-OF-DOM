feec10a070e9976c1f10eb517607d624bed34013
coder-utshab <ghoshutshob470@gmail.com>
2026-01-12 01:30:00 +0530

    cs

---
+++ stylein.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.paddingLeft = '10px';
+    section.style.backgroundColor = 'lightgray';
+} 
+
+const placescontainer = document.getElementById('places-container');
+placescontainer.style.backgroundColor = 'yellow';

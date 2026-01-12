36e6b6ed107ff91b99ddfcf6f0e3bf7c101c5076
coder-utshab <ghoshutshob470@gmail.com>
2026-01-12 17:59:48 +0530

    js

---
+++ first.js
@@ -0,0 +1,15 @@
+console.log('first file')
+
+const allLi = document.getElementsByTagName('li');
+console.log(allTitles);
+
+document.getElementsByClassName('section-title');
+console.log(allTitles);
+
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection);
+secondSection.style.color = 'violet';
+
+
+
---
+++ secoend.js
@@ -0,0 +1,13 @@
+
+// console.log('first file');
+
+// const allLi = document.getElementsByTagName('li');
+// console.log(allLi);
+
+// const allTitles = document.getElementsByClassName('section-title');
+// console.log(allTitles);
+
+// const secondSection = document.getElementById('second-section');
+// console.log(second-section);
+//     secondSection.style.color = 'violet';
+//     secondSection.style.backgroundColor = 'tomato'

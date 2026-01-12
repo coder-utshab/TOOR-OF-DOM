6f835c05c4ff547acaf2cefa0cc94a7cc647f7a1
coder-utshab <ghoshutshob470@gmail.com>
2026-01-12 16:41:57 +0530

    js

--- stylein.js
+++ stylein.js
@@ -9,7 +9,10 @@ for(const section of sections){
     section.style.borderRadius = '15px';
     section.style.paddingLeft = '10px';
     section.style.backgroundColor = 'lightgray';
+
 } 
 
+
 const placescontainer = document.getElementById('places-container');
-placescontainer.style.backgroundColor = 'blue';
+placescontainer.classList.add('text-center');
+placescontainer.classList.remove('large-text')
